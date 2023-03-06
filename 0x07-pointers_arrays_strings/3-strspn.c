377da42bbb924dfde7a9b5573a9b67205821a231
Techgeeko <agulonyemicheal9@gmail.com>
2023-03-06 13:35:48 -0800

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -21,12 +21,9 @@ unsigned int _strspn(char *s, char *accept)
 				break;
 			}
 			else if (accept[r + 1] == '\0')
-			{
 				return (n);
-			}
-			s++;
 		}
-		return (n);
+		s++;
 	}
-	return (0);
+	return (n);
 }
